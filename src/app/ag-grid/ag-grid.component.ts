9d73bf9d1aa056e1068b51ddb8014d4d9b55a6bc
Saeed <saeed.asghari.241@gmail.com>
2022-02-21 16:41:59 +0330

    change header to persain

--- src/app/ag-grid/ag-grid.component.ts
+++ src/app/ag-grid/ag-grid.component.ts
@@ -2,31 +2,34 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ColDef } from 'ag-grid-community';
 import { Observable } from 'rxjs';
-
 @Component({
   selector: 'app-ag-grid',
   templateUrl: './ag-grid.component.html',
-  styleUrls: ['./ag-grid.component.css']
+  styleUrls: ['./ag-grid.component.css'],
 })
+
+  
 export class AgGridComponent implements OnInit {
- // rowData: Observable<any[]>;
- lang : boolean = true;
+  // rowData: Observable<any[]>;
+  lang: boolean = true;
+
   constructor(private http: HttpClient) {
-  //  this.rowData = this.http.get<any[]>('https://www.ag-grid.com/example-assets/small-row-data.json');
-}
+    //  this.rowData = this.http.get<any[]>('https://www.ag-grid.com/example-assets/small-row-data.json');
+  }
 
   ngOnInit(): void {
+    
   }
-  columnDefs: ColDef[] = [
-    { field: 'سازنده',sortable: true ,filter: true, checkboxSelection: true},
-    { field: 'model' ,sortable: true,filter: true },
-    { field: 'price',sortable: true ,filter: true }
-];
 
-rowData = [
+  columnDefs: ColDef[] = [
+    {headerName: 'سازنده', field: 'make', sortable: true, filter: true, checkboxSelection: true },
+    { headerName: 'مدل',field: 'model', sortable: true, filter: true },
+    { headerName: 'قیمت',field: 'price', sortable: true, filter: true },
+  ];
+
+  rowData = [
     { make: 'Toyota', model: 'Celica', price: 35000 },
     { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxter', price: 72000 }
-];
-
+    { make: 'Porsche', model: 'Boxter', price: 72000 },
+  ];
 }
